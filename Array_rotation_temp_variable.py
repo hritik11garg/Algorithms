ed529ed0dfb27f7d3af32d285ff9e123501bdb94
hritik11garg <garghritikgarg@gmail.com>
2022-10-28 18:25:02 -0400

    Array rotation using temporary variable

--- Array_rotation_temp_variable.py
+++ Array_rotation_temp_variable.py
@@ -7,8 +7,7 @@ def Rotate(array, shift_by, size):
     for i in range (size - 1):
       array[i] = array[i + 1]
     array[size - 1] = last
-    p = p + 1
-     
+    p = p + 1  
      
 if __name__ == '__main__':
     array = [1, 2, 3, 4, 5, 6, 7]
