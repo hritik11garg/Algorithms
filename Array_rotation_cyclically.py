c079f789b8cae8c8b435b7aa91284cbbd2599fab
hritik11garg <garghritikgarg@gmail.com>
2022-10-28 23:20:28 -0400

    Array rotation done cyclically or move forward by one

--- Array_rotation_cyclically.py
+++ Array_rotation_cyclically.py
@@ -5,9 +5,9 @@ def rotate(arr, n):
     x = arr[n - 1]
      
     for i in range(n - 1, 0, -1):
-        arr[i] = arr[i - 1];
+        arr[i] = arr[i - 1]
          
-    arr[0] = x;
+    arr[0] = x
  
  
 # Main program
